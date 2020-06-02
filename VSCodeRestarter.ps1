26c92e53a7d41d98b82dc05bc4b3b17267a7ae69
CaptainChuckleFuck <Raymond388@icloud.com>
2020-06-02 16:32:31 +0100

    comment corrections

--- VSCodeRestarter.ps1
+++ VSCodeRestarter.ps1
@@ -1,5 +1,5 @@
 param ($projectFolderPath, $mainScriptPath)
-Get-Process | Where-Object { $_.MainWindowTitle -like '*Visual Studio Code' } | Stop-Process # Get old VS Code window as close it
+Get-Process | Where-Object { $_.MainWindowTitle -like '*Visual Studio Code' } | Stop-Process # Get old VS Code window and close it
 cmd.exe /c "code $projectFolderPath -g $mainScriptPath" # Start new VS Code window and open it on the main.py
 Start-Sleep 5 # let new window startup
 
@@ -7,6 +7,6 @@ Start-Sleep 5 # let new window startup
 $wshell = New-Object -ComObject wscript.shell;
 $wshell.AppActivate('Main.py - TwitchPlaysSpeedruns - Visual Studio Code')
 Start-Sleep 1 # let windows tab to the new screen
-$wshell.SendKeys("{F5}") # start debuggin command
+$wshell.SendKeys("{F5}") # start debugging command
 Start-Sleep 1
 $wshell.SendKeys("{ENTER}") # select debug as python (which is defualt option)
