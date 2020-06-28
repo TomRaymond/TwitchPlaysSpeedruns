869586fb0a33ff4a903b2d6aba240edb3c503e91
CaptainChuckleFuck <Raymond388@icloud.com>
2020-06-28 21:39:22 +0100

    New input controller integrated with message handler, combo keys logic imlpemented

--- inputcontroller.py
+++ inputcontroller.py
@@ -4,13 +4,20 @@ class KeyboardInput:
     repeat_delay = 0.05 # rate at which held inputs are repeated
 
     @staticmethod
-    def hold_for_seconds(seconds, *args):
+    def hold_for_seconds(keys, seconds):
         heldTime = 0
         while(heldTime < seconds):
-            for key in args: # tap every key down
+            for key in keys: # tap every key down
                 autopy.key.toggle(key, True)
             # delay before next tap and tick up our counter
             time.sleep(KeyboardInput.repeat_delay)
             heldTime += KeyboardInput.repeat_delay  
-        for key in args: # key up event for all keys
+        for key in keys: # key up event for all keys
                 autopy.key.toggle(key, False)
+
+class KeyInput:
+    keys = []
+    hold_time = 0
+    def __init__(self, keys, hold_time):
+        self.keys = keys
+        self.hold_time = hold_time
--- messagehandler.py
+++ messagehandler.py
@@ -1,9 +1,9 @@
 
 import twitch
 import yaml
-import keyholder
 import GetWindow
 import re
+import inputcontroller
 from datetime import date, datetime
 from restartcommand import RestartCommand
 
@@ -26,30 +26,67 @@ class MessageHandler:
 
     # commands that can only be run by admins
     def admin_commands(self, msg, user, channelChat):
-        if(user not in self.adminList):
-            return # if the are not a mod, do nothing else
-        if msg == "modcheck": channelChat.send('you are a mod')
+        if(user not in self.adminList): return # if the are not a mod, do nothing else
         self._run_command(self.adminCommands, msg)
 
-    def _run_command(self, commandList, message):
-        givenInput = message
-        givenTime = None
-        if(':' in message):
-            splitMessage = message.split(':')
-            givenInput = splitMessage[0]
-            givenTime = self.convert_time_input(splitMessage[1])
-                
+    def _run_command(self, commandList, message): 
+        keyboardInput = self.parse_input(commandList, message)
+        if(keyboardInput.keys == []): return # If input was invalid (had no matching keys) then return 
+
+        self.emulatorWindow.make_active() # ensure window is active before sending key
+        inputcontroller.KeyboardInput.hold_for_seconds(keyboardInput.keys, keyboardInput.hold_time)
+
+    # looks up the given command, returns blank key input if none found
+    def resolve_single_input(self, commandList, givenCommand):
         for command in commandList:
             for alias in command['aliases']:
-                if alias == givenInput: # check if this is a direct match to the key
-                    self.emulatorWindow.make_active() # ensure window is active before seding key
-                    # pass the key for the match key alias to the heyholder
-                    if givenTime == None:
-                        givenTime = command['time'] 
-                    print("    pressed " + str(command['key']) + " for " + str(givenTime) + " seconds")
-                    for key in command['key']:
-                        keyholder.holdForSeconds(key, givenTime)
-                    return # do nothing else
+                if alias == givenCommand: # check if this is a direct match to the key
+                    keyInput = inputcontroller.KeyInput(command['key'], command['time'])
+                    return keyInput # do nothing else
+        return inputcontroller.KeyInput([], 0)
+
+    def parse_input(self, commandList, givenInput): # [m+k]:1  k:2 j        
+        givenTime = None        
+        commandStatement = givenInput
+        if(':' in givenInput): # Check for custom defined key press time
+                splitMessage = givenInput.split(':')
+                commandStatement = splitMessage[0]
+                givenTime = self.convert_time_input(splitMessage[1])
+
+        commandStatement = commandStatement.translate({ord(c):None for c in ' \n\t\r'}) # strip whitespace
+        multiKeypress = re.findall(r'(?<=\[)[\w\d\+]+(?=\])', givenInput) # match any letters, numbers or '+' that fall between a '[' and a ']'
+        
+        keyInput = None
+        if(len(multiKeypress) == 1): # if there is a single '[' to ']' match
+            keyInput = self.resolve_multikey_input(commandList, multiKeypress[0])
+        else:
+            keyInput = self.resolve_single_input(commandList, commandStatement)
+        
+        if(givenTime != None):
+            keyInput.hold_time = givenTime
+        return keyInput
+
+    def resolve_multikey_input(self, commandList, multiInput):
+        splitMultiInput = multiInput.split('+')
+        resolvedKeys = []
+        for singleInput in splitMultiInput:
+            resolvedKeys.append(self.resolve_single_input(commandList, singleInput))
+
+        multiKeyInput = inputcontroller.KeyInput([], 0)
+        for resolvedKey in resolvedKeys: # add all resolved keys into a single keyboard keyhold command. 
+            multiKeyInput.keys.extend(resolvedKey.keys)
+            if(multiKeyInput.hold_time < resolvedKey.hold_time): # the combined multi keypress will hold for the longest defined time
+                multiKeyInput.hold_time = resolvedKey.hold_time
+
+        return multiKeyInput
+
+    def parse_hold_time(self, givenInput):
+            if(':' in givenInput):
+                splitMessage = givenInput.split(':')
+                return self.convert_time_input(splitMessage[1])
+            else:
+                return None        
+
     def convert_time_input(self, time):        
         try:
             convertedTime = float(time)
@@ -118,12 +155,23 @@ class MessageHandler:
         with open("LogFile.txt", 'w') as logFile:
             logFile.write("The oracle bestows knowledge from the great hero " + str(user) + " and tells you: " + str(logMessage))
             channel.send("The Oracle appreciates your wisdom")
-        
 
-    def bot_commands(self, msg, user, channel):        
-        if(self.multi_command(msg, user, channel)): return # if handled by multi command, do nothing else
-        if(self.restart_script(msg, user)): return # if handled by restart_script, do nothing else
-        if(self.action_log(msg, user, channel)): return
+    def mod_check(self, msg, user, channel):
+        if (msg == "!modcheck"):
+            if(user in self.adminList):
+                channel.send(str(user) + ' you are a mod')
+            else:
+                channel.send(str(user) + ' you are not a mod')
+
+            return True # this was a modcheck command so return true
+        return False
+    
+    def bot_commands(self, msg, user, channel):       
+        # each function returns false unless the 'msg' matches it's command word
+        if(self.mod_check(msg, user, channel)): return # !modcheck
+        if(self.multi_command(msg, user, channel)): return # !m / !multi
+        if(self.restart_script(msg, user)): return # !restartscript
+        if(self.action_log(msg, user, channel)): return # !oracle
 
     # can be subscribed to the twitch bot to receive messages when sent in twitch chat
     def receive_twitch_message(self, message: twitch.chat.Message) -> None:
