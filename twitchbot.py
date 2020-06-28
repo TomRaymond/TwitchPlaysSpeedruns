b4e93cd689140af5fb7b5970227db2c25c1ecc78
CaptainChuckleFuck <Raymond388@icloud.com>
2020-06-28 18:15:51 +0100

    bug fix

--- twitchbot.py
+++ twitchbot.py
@@ -30,7 +30,7 @@ class TwitchBot:
         self.chat.irc.active = False
         self.chat.dispose()
         self.connect_chat(self.lastLogin)
-        print("No input for "+ self._countdownPeriod +" seconds. Refreshing Connection to Twitch.")
+        print("No input for "+ str(self._countdownPeriod )+" seconds. Refreshing Connection to Twitch.")
 
     def receive_twitch_message(self, message: twitch.chat.Message) -> None:
         self.reconnectionCountdown.reset() # if a message is received, reset the connection
